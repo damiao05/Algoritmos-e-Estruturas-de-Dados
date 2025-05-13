6d024937ae88ae10dac7650f90bf438d6dae2e3a
Arthur Damião <138040667+damiao05@users.noreply.github.com>
2025-05-13 14:48:03 -0300

    Add files via upload

---
+++ UsandoJUnit/src/calculadora/Calculadora.java
@@ -0,0 +1,30 @@
+package calculadora;
+
+public class Calculadora {
+
+	public Double soma(double d, double e) {
+		// TODO Auto-generated method stub
+		return d + e;
+	}
+
+	public Double subtracao(double d, double e) {
+		// TODO Auto-generated method stub
+		return d - e;
+	}
+
+	public Double multipliacacao(double d, double e) {
+		// TODO Auto-generated method stub
+		return d * e;
+	}
+
+	public Double divisao(double d, double e) {
+		// TODO Auto-generated method stub
+		return d / e;
+	}
+
+	public Double potenciacao(double d, double e) {
+		// TODO Auto-generated method stub
+		return Math.pow(d, e);
+	}
+
+}
